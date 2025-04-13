ddc0588cf7cb440c496f2560964282a81fe10f4f
achiveIt <154082077+achiveIt@users.noreply.github.com>
2025-04-13 16:09:21 +0530

    feat: add createLink page (#15)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import CreateLink from './pages/createLink';
 import Login from './pages/Login';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create" element={<CreateLink />} />
       </Routes>
     </Router>
   );
